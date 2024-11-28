90c9af42fd9f3c89eb1e8c6f4d9af2bd3bfbfda5
Alok Dake <alokdake2003@gmail.com>
2024-11-28 17:24:17 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Alok Ashok Dake Virat Kohli</h1>
+      <h1>Virat Kohli</h1>
       <div>Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok </div>
     </div>
   );
