a7fc14d1128aed1a2c87896d21585002a523cce9
Alok Dake <alokdake2003@gmail.com>
2024-11-28 16:51:42 +0530

    1st commit

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM node
 WORKDIR /src
 COPY . .
 RUN npm install
+EXPOSE 4000
 CMD npm start
--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Alok Ashok Dake</h1>
+      <h1>Alok Ashok Dake Virat Kohli</h1>
       <div>Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok Alok </div>
     </div>
   );
