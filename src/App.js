3d88ef57ff0962ddbde6791580953ecd79cefe85
Alok Dake <alokdake2003@gmail.com>
2024-11-28 23:00:53 +0530

    d

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>Virat Kohli is Greated of All Time (GOAT) CIRCKERTER</h1>
-      <div>Alok Ashok Dake Alok Ashok Dake Alok Ashok Dake Alok Ashok DakeAlok Ashok Dake </div>
+      <div>Alok Ashok Dake 
+        SUNBEAM KARAD CDAC 
+        DEVOPS
+        JINKINES
+         </div>
     </div>
   );
 }
